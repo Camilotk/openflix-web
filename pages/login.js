b908955989df78fe6dc6d357a6bd3528d4bf155e
Camilo Cunha de Azevedo <Camilotk@gmail.com>
2021-03-25 10:49:59 -0300

    :passport_control: Created the Login Page
    Created the login page for the system

---
+++ pages/login.js
@@ -0,0 +1,20 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+import { useEffect, useState } from 'react'
+import Layout from '../components/layout/Layout'
+import LoginForm from '../components/shards/LoginForm'
+import { render } from 'react-dom'
+import axios from 'axios'
+
+
+export default function Login() {
+    const urls = {
+        logo: "https://camo.githubusercontent.com/629aa0f8d1cfc1b33d2f0572b7901cf11321d2ba7916925a87fbd566b39f5e95/68747470733a2f2f692e6962622e636f2f5a634b7135526b2f6f70656e666c69782d6c6f676f2e706e67"
+      }
+  
+  return (
+    <Layout imgs={urls}>
+      <LoginForm />
+    </Layout>
+  )
+}
